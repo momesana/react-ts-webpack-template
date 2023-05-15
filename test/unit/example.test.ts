d13fc3b77e0cd6f866f8044a7b8e2b9f71bdf277
Mohammad Mehdi Salem Naraghi <momesana@gmail.com>
2023-05-16 00:20:23 +0200

    ci: enable unit testing via jest

---
+++ test/unit/example.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("example test", () => {
+  test("used as a placeholder to test the CI until we have proper tests", () => {
+    expect(true).toBe(true);
+  });
+});
